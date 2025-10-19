99349f7e8148c5626350137ddb0a02da6a1af80d
尹硕 <2687337144@qq.com>
2025-10-19 20:16:37 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <Home />
+    <!-- <Home> </Home> -->
+
+  </div>
+</template>
+
+
+<script>
+import  Home  from './views/Home.vue';
+
+export default{
+  name:'App',
+  components:{
+    Home,
+  },
+};
+
+
+
+</script>
+
+
+<style >
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background-color: #000;
+  color: #fff;
+  font-family: 'Inter', system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+.app-container {
+  position: relative;
+  min-height: 100vh;
+  width: 100%;
+  overflow-x: hidden;
+  background-color: #000;
+}
+</style>
---
+++ src/views/home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home-container">
+    <h1>VUE3教学</h1>
+    <hr>
+    <Mobanyufa></Mobanyufa>
+    <Shuxingbangding></Shuxingbangding>
+    <Tiaojianxuanran></Tiaojianxuanran>
+  </div>
+</template>
+
+
+<script>
+import Mobanyufa from '../components/mobanyufa.vue';
+import Shuxingbangding from '../components/shuxingbangding.vue';
+import Tiaojianxuanran from '../components/tiaojianxuanran.vue';
+
+export default {
+    name: 'home',
+    components: {
+        Mobanyufa,
+        Shuxingbangding,
+        Tiaojianxuanran
+    },
+}
+
+</script>
+
+<style scoped>
+.home-container{
+    position:relative;
+    z-index:10;
+}
+
+h1{
+    z-index:11;
+    color:aqua;
+}
+
+</style>
